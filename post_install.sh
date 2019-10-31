a3b3555837f779605dc930a51c15c65c8d8bd394
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-10-31 11:30:44 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+fetch https://services.sonarr.tv/v1/download/phantom-develop/latest?version=3&os=linux -o /usr/local/share
+tar -xzvf /usr/local/share/Sonarr.phantom-develop.*.linux.tar.gz -C /usr/local/share"
+rm /usr/local/share/Sonarr.phantom-develop.*.linux.tar.gz
+
+pw user add sonarr -c sonarr -u 351 -d /nonexistent -s /usr/bin/nologin
+pw groupadd -n mnt-tvshows -g 1002
+pw groupmod 1002 -m 351
+
+chmod u+x /etc/rc.d/sonarr
+
 sysrc -f /etc/rc.conf sonarr_enable="YES"
 
 #62991
